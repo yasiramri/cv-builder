fea0c0095bed2822345d105c2ce9c78d375f0ac8
Muhammad Yasir Amri <yasiramrr@gmail.com>
2025-05-24 09:07:27 +0700

    cv builder

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,29 @@ import Home from './pages/Home';
 import CVBuilderPage from './pages/CVBuilderPage';
 import CVtotal from './pages/CVtotal';
 
+// Komponen sederhana untuk 404
+function NotFoundPage() {
+  return (
+    <div style={{ textAlign: 'center', padding: 40 }}>
+      <h1>404 Not Found</h1>
+      <img
+        src="https://http.cat/404"
+        alt="404 Not Found"
+        style={{ maxWidth: 400, width: '100%' }}
+      />
+      <p>Oops, halaman yang kamu cari tidak ditemukan.</p>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cv-list" element={<CVtotal />} />
+        {/* Route 404 Not Found harus di paling bawah */}
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </div>
   );
